7f2c3dba11399e28f646ffeb699e47fdbbc64e5b
Yakapi <realitygameserver@gmail.com>
2022-08-10 20:56:02 +0200

    FE init Going for test on V env

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-// Import the functions you need from the SDKs you need
-// const firebase = require("firebase/admin")
-var test = require("firebase/admin")
-console.log(test);
 var serviceAccount = require("./portfolio-29765-firebase-adminsdk-tvk6j-96416ba553.json");
-var admin = require("firebase/app");
-var firestore = require("firebase/firestore")
-const firebaseConfig = {
-  apiKey: "AIzaSyDpwWZdFJNMj-lNKVKcvXdIiQE__2IZI2c",
-  authDomain: "portfolio-29765.firebaseapp.com",
-  projectId: "portfolio-29765",
-  storageBucket: "portfolio-29765.appspot.com",
-  messagingSenderId: "509117271453",
-  appId: "1:509117271453:web:673ee064be773c26cd3374",
-  measurementId: "G-RKCTZ0P7NZ"
-};
-// const app_fire = admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-const app_fire = admin.initializeApp(firebaseConfig);
-let db = firestore.getFirestore(app_fire)
+var admin = require("firebase-admin");
+const app_fire = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-// console.log(firestore.collection(db, "users"));
-  let user = firestore.collection(db, "users")
-  // let user_doc = firestore.getDocs(user)
-  console.log(user);
-//  async function getCities(db) {
-//   const citiesCol = firestore.collection(db, 'users');
-//   const citySnapshot = await firestore.getDocs(citiesCol);
-//   const cityList = citySnapshot.docs.map(doc => doc.data());
-//   return cityList;
-// }
-// let test = getCities(db)
-// console.log(test);
-//
-// const db = admin.firestore()
+const db = admin.firestore();
 
-// let user = db.collections("users")
-// console.log(user);
-// console.log(test)
-// Initialize Firebase
-// const db_firebase = firebase.firestore()
-// const analytics = getAnalytics(app_fire);
-// Add Express
-// const express = require("express");
-//
-// // Initialize Express
-// const app = express();
-//
-// // Create GET request
-// app.get("/", (req, res) => {
-//   res.send("Express on Vercel");
-// });
-//
-// // Initialize server
-// app.listen(5000, () => {
-//   console.log("Running on port 5000.");
-// });
+// appel du table entière
+async function getAllUsers(db){
+  const users = db.collection("users")
+  users.get().then((querySnapshot) =>{
+    querySnapshot.forEach((document) => {
+      // document.data();
+      console.log(document.data());
+    });
+
+  })
+}
+//appel un seul element d'une table
+async function getUser(){
+
+  const user_alone = db.collection("users").doc("Wvnd9oO5aUFw1lfzggg4")
+  const user_element = await user_alone.get()
+  if (!user_element.exists) {
+    console.log('No such document!');
+  } else {
+      let user_doc = {
+        name: user_element.data().name,
+        admin: user_element.data().admin,
+        u_id: user_element.data().u_id
+      }
+      return user_doc
+  }
+}
+
+
+//Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+app.get("/user", async(req, res, next) => {
+  let test =   await getUser(db)
+  res.json(test);
+});
+app.get("/users", (req, res) => {
+  res.send(getAllUsers(db)
+);
+});
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
