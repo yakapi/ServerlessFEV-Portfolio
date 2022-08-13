622ba074cd75fd0b107a422cd1f29e43c26e8ef7
Yakapi <realitygameserver@gmail.com>
2022-08-13 12:03:57 +0200

    Test .env

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var serviceAccount = require("./portfolio-29765-firebase-adminsdk-tvk6j-96416ba553.json");
+// var serviceAccount = require("./portfolio-29765-firebase-adminsdk-tvk6j-96416ba553.json");
+var serviceAccount = {
+  "type": process.env.firebase_type,
+  "project_id": process.env.firebase_project_id,
+  "private_key_id": process.env.firebase_private_key_id,
+  "private_key": process.env.firebase_private_key,
+  "client_email": process.env.firebase_client_email,
+  "client_id": process.env.firebase_client_id,
+  "auth_uri": process.env.firebase_auth_uri,
+  "token_uri": process.env.firebase_token_uri,
+  "auth_provider_x509_cert_url": process.env.firebase_auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.firebase_client_x509_cert_url
+}
 
 var admin = require("firebase-admin");
 const express = require("express");
